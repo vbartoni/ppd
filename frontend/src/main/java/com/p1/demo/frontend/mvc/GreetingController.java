244c4fd74217506bed5f94909e4b90a9b2decc42
Vedran Bartonicek <vbartoni@gmail.com>
2015-08-18 18:46:45 +0300

    tiny cleanup

--- frontend/src/main/java/com/p1/demo/frontend/mvc/GreetingController.java
+++ frontend/src/main/java/com/p1/demo/frontend/mvc/GreetingController.java
@@ -3,7 +3,7 @@ package com.p1.demo.frontend.mvc;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.p1.demo.frontend.domain.Greeting;
-import com.p1.demo.frontend.service.DummyService;
+import com.p1.demo.frontend.service.ProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +15,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
     @Autowired
-    DummyService ds;
+    ProducerService ds;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
